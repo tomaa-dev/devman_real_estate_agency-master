7a0654580700db56cefa81cbf11de349503db939
tomaa-dev <lajpanovatoma@gmail.com>
2026-06-15 21:19:33 +0300

    Change related_name

--- property/models.py
+++ property/models.py
@@ -49,7 +49,7 @@ class Flat(models.Model):
         db_index=True)
     liked_by = models.ManyToManyField(
         User,
-        related_name='liked_posts',
+        related_name='liked_flats',
         verbose_name='Кто лайкнул'
     )
 
